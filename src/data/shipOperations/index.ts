a539870477746ad2b7a5f72443f6aadfbead6f98
alperrenoztrk <ozturkalperen308@gmail.com>
2026-07-12 13:44:08 +0300

    Register Offshore DP operations

--- src/data/shipOperations/index.ts
+++ src/data/shipOperations/index.ts
@@ -4,10 +4,11 @@ import { roRo } from "./roRo";
 import { tanker } from "./tanker";
 import { dokme } from "./dokme";
 import { yolcu } from "./yolcu";
+import { offshore } from "./offshore";
 
 export type { DepartmentId, ShipType, ShipOperation, ShipOperationDepartment } from "./types";
 
-export const shipTypes: ShipType[] = [konteyner, roRo, tanker, dokme, yolcu];
+export const shipTypes: ShipType[] = [konteyner, roRo, tanker, dokme, yolcu, offshore];
 
 export const shipTypeMap: Record<string, ShipType> = Object.fromEntries(
   shipTypes.map((s) => [s.id, s]),
